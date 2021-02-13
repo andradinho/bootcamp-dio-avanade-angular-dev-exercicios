c1c0939e0af0dc8e79d0325f7a214f87ac8ddee4
Andradinho <raphaelandrade2013@gmail.com>
2021-02-13 14:14:22 -0300

    Praticando - Condicionais, laços de repetição e Date.

--- programacao-para-internet-com-javascript/js/main.js
+++ programacao-para-internet-com-javascript/js/main.js
@@ -1,13 +1,43 @@
-var fruta = {nome:"maça", cor:"vermelha"}
-console.log(fruta)
-console.log(fruta.nome)
-console.log(fruta.cor)
+
+var lista = ["ale", "in"]/
+lista.push("pol");
+
+// var d = new Date();
+// alert(d);
+
+//var count;
+//for(count=0; count<=5; count++){
+//    alert(count);
+//};
+
+// var count = 5;
+// while(count < 5){
+//     console.log(count);
+//     count = count + 1; 
+// };
+
+
+// var idade = prompt("Qual sua idade?");
+// var idade = 18;
+// if(idade >= 18){
+//     alert("maior de idade");
+// }else{
+//     alert("menor de idade");
+// };
+
+
+// var fruta = {nome:"maça", cor:"vermelha"}
+// console.log(fruta)
+// console.log(fruta.nome)
+// console.log(fruta.cor)
+
 
 // var lista = ["maça", "pera", "laranja"];
 // lista.push("uva");
 // console.log(lista);
 // console.log(lista.toString());
 // console.log(lista.pop());
+
 
 // var nome = "Raphael Andrade";
 // var idade = 28;
