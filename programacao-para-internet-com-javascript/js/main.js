db7689c95afc5f9c4ce18c02b9e0cde9b1f2eb14
Andradinho <raphaelandrade2013@gmail.com>
2021-02-13 14:21:37 -0300

    Praticando - Desenvolva páginas web com JavaScript.

--- programacao-para-internet-com-javascript/js/main.js
+++ programacao-para-internet-com-javascript/js/main.js
@@ -1,6 +1,27 @@
 
-var lista = ["ale", "in"]/
-lista.push("pol");
+
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+// 
+// function validaIdade(idade){
+//     var validar;
+//     if(idade >= 18){
+//         validar = true
+//     }else{
+//         validar = false
+//     }
+//     return validar;
+// }
+// 
+// var idade = prompt("Qual sua idade?")
+// console.log(validaIdade(idade))
+// 
+// alert(soma(5, 10))
+// 
+
+
 
 // var d = new Date();
 // alert(d);
