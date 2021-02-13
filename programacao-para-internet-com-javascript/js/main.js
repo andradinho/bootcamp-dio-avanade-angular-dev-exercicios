0f177d379c7a6f315ded872883cf130bfeec5fe2
Andradinho <raphaelandrade2013@gmail.com>
2021-02-13 13:53:52 -0300

    Praticando - Introdução ao JavaScript.

---
+++ programacao-para-internet-com-javascript/js/main.js
@@ -0,0 +1,17 @@
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.pop());
+
+// var nome = "Raphael Andrade";
+// var idade = 28;
+// var idade2 = 1;
+// var frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " + (idade + idade2) + " anos.");
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log(frase);
+// console.log(frase.replace("Japão", "Brasil"));
+// console.log(frase.toUpperCase());
