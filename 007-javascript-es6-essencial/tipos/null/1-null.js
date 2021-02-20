77d97ba7648396d2bcd812768b4732f3b2407e01
Andradinho <raphaelandrade2013@gmail.com>
2021-02-20 11:16:00 -0300

    Entendendo o type null.

---
+++ 007-javascript-es6-essencial/tipos/null/1-null.js
@@ -0,0 +1,4 @@
+const nullVariable = null;
+
+console.log(typeof nullVariable);
+// Ainda que o tipo esteja setado como null, ele sempre será um object.
--- 007-javascript-es6-essencial/tipos/number/1-number.js
+++ 007-javascript-es6-essencial/tipos/number/1-number.js
@@ -2,14 +2,14 @@ const myNumber = 12.4032;
 
 // Transformar número para string
 const numberAsString = myNumber.toString();
-console.log('Número transformado em string:', typeof numberAsString);
+console.log('Número transformado em string:' + ' tipo: ' + typeof numberAsString + '  Valor: ' + numberAsString);
 
-// Retorna número com um número de casas decimais
+// Retorna número definindo a quantidade de casas decimais
 const fixedTwoDecimals = myNumber.toFixed(2);
 console.log('\nNúmero com duas casas decimais:', fixedTwoDecimals);
 
 // Transforma uma string em float
-console.log('\nString parseada para float:', parseFloat('13.22'));
+console.log('\nString convertida para float:', parseFloat('13.22'));
 
 // Transforma uma string em int
 console.log('\nString parseada para int:', parseInt('13.20'));
